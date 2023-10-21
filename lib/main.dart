6d1ec8edf16577fc31fc2645b9677534f00b5157
serge-eric-kalaga <kalagaserge4@gmail.com>
2023-10-21 22:38:35 +0000

    init app

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase TaskList',
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+          child: Container(
+        decoration: BoxDecoration(color: Colors.black45),
+      )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
