63bf1d1c5e53fb170a85db07225433c3af5baef1
serge-eric-kalaga <kalagaserge4@gmail.com>
2023-10-23 08:57:21 +0000

    list de tache de base ok

--- lib/main.dart
+++ lib/main.dart
@@ -46,59 +46,67 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-          child: Container(
-        decoration: BoxDecoration(color: Colors.black45),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  margin: EdgeInsets.all(10),
-                  width: double.infinity,
-                  child: TextField(
-                    controller: textEditingController,
-                    autocorrect: true,
+          child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          decoration: BoxDecoration(color: Colors.black45),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    margin: EdgeInsets.all(10),
+                    width: double.infinity,
+                    child: TextField(
+                      controller: textEditingController,
+                      autocorrect: true,
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.black54)),
-                    onPressed: () {
-                      setState(() {
-                        tasks.add(Task(name: textEditingController.text));
-                      });
-                    },
-                    child: Text("Add task"),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(5, 20, 5, 0),
-              height: MediaQuery.of(context).size.height * 0.6,
-              child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, index) => ListTile(
-                  leading: Checkbox(
-                      value: tasks[index].isCompleted,
-                      onChanged: (value) {
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.black54)),
+                      onPressed: () {
                         setState(() {
-                          tasks[index].isCompleted = value;
+                          tasks.add(Task(name: textEditingController.text));
                         });
-                      }),
-                  title: Text('${tasks[index].name}'),
-                ),
+                      },
+                      child: Text("Add task"),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
+              Container(
+                margin: EdgeInsets.fromLTRB(5, 20, 5, 0),
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) => ListTile(
+                    leading: Checkbox(
+                        value: tasks[index].isCompleted,
+                        onChanged: (value) {
+                          setState(() {
+                            tasks[index].isCompleted = value;
+                          });
+                        }),
+                    title: Text(
+                      '${tasks[index].name}',
+                      style: tasks[index].isCompleted == true
+                          ? TextStyle(decoration: TextDecoration.lineThrough)
+                          : null,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       )), // This trailing comma makes auto-formatting nicer for build methods.
     );
